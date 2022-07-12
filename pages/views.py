adf7c8868aef12b8504393b68408e1ffdb081d05
chimaek <pipiru100@gmail.com>
2022-07-12 22:06:03 +0900

    push

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
 
-
 # Create your views here.
 
 
 def index(req):
-    return render(req, 'template/pages/index.html')
+    return render(req, 'pages/index.html')
 
 
 def about(req):
-    return render(req, 'template/pages/about.html')
+    return render(req, 'pages/about.html')
